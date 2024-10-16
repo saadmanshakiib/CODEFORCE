d9148f8067dd83b55e7b247a55d91ab18eb79c38
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2024-10-16 23:58:42 +0600

    Add files via upload

--- boyorgirl.cpp
+++ boyorgirl.cpp
@@ -4,13 +4,13 @@ int main(){
     string s;
     cin>>s;
     set<char>S;
-    for(int i=0;i<S.size();i++){
-        S.insert(s[i]);
+    for(char c : s){
+        S.insert(c);
     }
-    if(S.size()%2==1){
-    cout<<"IGNORE HIM!"<<endl;
+    if(S.size()%2==0){
+    cout<<"CHAT WITH HER!"<<endl;
     }
     else{
-        cout<<"CHAT WITH HER!"<<endl;
+        cout<<"IGNORE HIM!"<<endl;
     }
 }
