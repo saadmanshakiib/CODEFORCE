c514c40302206cd35ef96a229f3152b068315337
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2024-10-23 20:17:31 +0600

    Add files via upload

---
+++ 1703A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        if (s == "yes")
+            cout << "YES" << endl;
+        else 
+            cout << "NO" << endl;
+    }
+
+}
