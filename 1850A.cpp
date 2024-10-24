aa090adcb94cf72bd78dfdddfec22f28ee5924a7
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2024-10-24 11:40:35 +0600

    Add files via upload

---
+++ 1850A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b >= 10) cout<<"YES"<<endl;
+        else if(b+c >= 10) cout<<"YES"<<endl;
+        else if(a+c >= 10) cout<<"YES"<<endl;
+        else{
+            cout<<"NO"<<endl;
+        }
+
+    }
+}
