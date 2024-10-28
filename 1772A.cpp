d94b7af7f42f2b8cb06cc04f06a68d964d540003
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2024-10-28 11:48:06 +0600

    Add files via upload

---
+++ 1772A.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;cin>>t;while(t--){
+        char a,p,b;
+        cin>>a>>p>>b;
+        int r = (a-'0')+(b-'0');
+        cout<<r<<endl;
+    }
+}
