006c2ed3f7586247fb0f3684b1edb2334f1a3b4b
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2024-11-13 20:37:43 +0600

    Add files via upload

---
+++ petyastring.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+string s1,s2;cin>>s1>>s2;
+for(char &c : s1){
+    c = tolower(c);
+}
+for(char &c : s2){
+    c = tolower(c);
+}
+if(s1>s2)cout<<"1"<<endl;
+else if(s2>s1) cout<<"-1"<<endl;
+else cout<<"0"<<endl;
+}
